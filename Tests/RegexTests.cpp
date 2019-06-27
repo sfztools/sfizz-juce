f45088b2eb0329cd732a1f18417e03a6d0e261b1
Paul Ferrand <paulfd@outlook.fr>
2019-06-27 05:30:55 +0200

    Added tests for the define regex

--- Tests/RegexTests.cpp
+++ Tests/RegexTests.cpp
@@ -3,45 +3,53 @@
 #include "../Source/SfzGlobals.h"
 using namespace Catch::literals;
 
-TEST_CASE("Include Regex", "Regex Tests")
+void includeTest(const std::string& line, const std::string& fileName)
 {
-    auto filenameTest = [](const std::string& fileName){
-        SECTION(fileName)
-        {
-            std::smatch includeMatch;
-            std::string testString { "#include \"" + fileName + "\"" };
-            auto found = std::regex_search(testString, includeMatch, SfzRegexes::includes);
-            REQUIRE(found);
-            REQUIRE(includeMatch[1] == fileName);
-        }
-    };
+    std::smatch includeMatch;
+    auto found = std::regex_search(line, includeMatch, SfzRegexes::includes);
+    REQUIRE(found);
+    REQUIRE(includeMatch[1] == fileName);
+}
 
-    filenameTest("file.sfz");
-    filenameTest("../Programs/file.sfz");
-    filenameTest("..\\Programs\\file.sfz");
-    filenameTest("file-1.sfz");
-    filenameTest("file~1.sfz");
-    filenameTest("file_1.sfz");
-    filenameTest("file$1.sfz");
-    filenameTest("file,1.sfz");
+TEST_CASE("#include Regex", "Regex Tests")
+{
+    includeTest("#include \"file.sfz\"", "file.sfz");
+    includeTest("#include \"../Programs/file.sfz\"", "../Programs/file.sfz");
+    includeTest("#include \"..\\Programs\\file.sfz\"", "..\\Programs\\file.sfz");
+    includeTest("#include \"file-1.sfz\"", "file-1.sfz");
+    includeTest("#include \"file~1.sfz\"", "file~1.sfz");
+    includeTest("#include \"file_1.sfz\"", "file_1.sfz");
+    includeTest("#include \"file$1.sfz\"", "file$1.sfz");
+    includeTest("#include \"file,1.sfz\"", "file,1.sfz");
+    includeTest("#include \"rubbishCharactersAfter.sfz\" blabldaljf///df", "rubbishCharactersAfter.sfz");
+    includeTest("#include \"lazyMatching.sfz\" b\"", "lazyMatching.sfz");
+}
 
-    SECTION("Whitespace and rubbish")
-    {
-        std::smatch includeMatch;
-        std::string fileName { "file.sfz" };
-        std::string testString { "#include \"" + fileName + "\" blabldaljf///df" };
-        auto found = std::regex_search(testString, includeMatch, SfzRegexes::includes);
-        REQUIRE(found);
-        REQUIRE(includeMatch[1] == fileName);
-    }
+void defineTest(const std::string& line, const std::string& variable, const std::string& value)
+{
+    std::smatch defineMatch;
+    auto found = std::regex_search(line, defineMatch, SfzRegexes::defines);
+    REQUIRE(found);
+    REQUIRE(defineMatch[1] == variable);
+    REQUIRE(defineMatch[2] == value);
+}
 
-    SECTION("Check for lazy matching")
-    {
-        std::smatch includeMatch;
-        std::string fileName { "file.sfz" };
-        std::string testString { "#include \"" + fileName + "\" b\"" };
-        auto found = std::regex_search(testString, includeMatch, SfzRegexes::includes);
-        REQUIRE(found);
-        REQUIRE(includeMatch[1] == fileName);
-    }
+void defineFails(const std::string& line)
+{
+    std::smatch defineMatch;
+    auto found = std::regex_search(line, defineMatch, SfzRegexes::defines);
+    std::cout << line << '\n';
+    REQUIRE(!found);
+}
+
+TEST_CASE("#define Regex", "Regex Tests")
+{
+    defineTest("#define $number 1", "$number", "1");
+    defineTest("#define $letters QWERasdf", "$letters", "QWERasdf");
+    defineTest("#define $alphanum asr1t44", "$alphanum", "asr1t44");
+    defineTest("#define  $whitespace   asr1t44   ", "$whitespace", "asr1t44");
+    defineTest("#define $lazyMatching  matched  bfasd ", "$lazyMatching", "matched");
+    defineFails("#define $symbols# 1");
+    defineFails("#define $symbolsAgain $1");
+    defineFails("#define $trailingSymbols 1$");
 }
