bf54a3efc28446f6de1ed16b594498c86368a312
Paul Ferrand <paulfd@outlook.fr>
2019-06-28 16:15:20 +0200

    Finished the regex tests

--- Tests/RegexTests.cpp
+++ Tests/RegexTests.cpp
@@ -38,7 +38,6 @@ void defineFails(const std::string& line)
 {
     std::smatch defineMatch;
     auto found = std::regex_search(line, defineMatch, SfzRegexes::defines);
-    std::cout << line << '\n';
     REQUIRE(!found);
 }
 
@@ -52,4 +51,83 @@ TEST_CASE("#define Regex", "Regex Tests")
     defineFails("#define $symbols# 1");
     defineFails("#define $symbolsAgain $1");
     defineFails("#define $trailingSymbols 1$");
+}
+
+TEST_CASE("Header Regex", "Regex Tests")
+{
+    SECTION("Basic header match")
+    {
+        std::smatch headerMatch;
+        std::string line{"<header>param1=value1 param2=value2<next>"};
+        auto found = std::regex_search(line, headerMatch, SfzRegexes::headers);
+        REQUIRE(found);
+        REQUIRE(headerMatch[1] == "header");
+        REQUIRE(headerMatch[2] == "param1=value1 param2=value2");
+    }
+    SECTION("EOL header match")
+    {
+        std::smatch headerMatch;
+        std::string line{"<header>param1=value1 param2=value2"};
+        auto found = std::regex_search(line, headerMatch, SfzRegexes::headers);
+        REQUIRE(found);
+        REQUIRE(headerMatch[1] == "header");
+        REQUIRE(headerMatch[2] == "param1=value1 param2=value2");
+    }
+}
+
+void memberTest(const std::string &line, const std::string &variable, const std::string &value)
+{
+    std::smatch memberMatch;
+    auto found = std::regex_search(line, memberMatch, SfzRegexes::members);
+    REQUIRE(found);
+    REQUIRE(memberMatch[1] == variable);
+    REQUIRE(memberMatch[2] == value);
+}
+
+TEST_CASE("Member Regex", "Regex Tests")
+{
+    memberTest("param=value", "param", "value");
+    memberTest("param=113", "param", "113");
+    memberTest("param1=value", "param1", "value");
+    memberTest("param_1=value", "param_1", "value");
+    memberTest("param_1=value", "param_1", "value");
+    memberTest("ampeg_sustain_oncc74=-100", "ampeg_sustain_oncc74", "-100");
+    memberTest("lorand=0.750", "lorand", "0.750");
+    memberTest("sample=value", "sample", "value");
+    memberTest("sample=value-()*", "sample", "value-()*");
+    memberTest("sample=../sample.wav", "sample", "../sample.wav");
+    memberTest("sample=..\\sample.wav", "sample", "..\\sample.wav");
+    memberTest("sample=subdir\\subdir\\sample.wav", "sample", "subdir\\subdir\\sample.wav");
+    memberTest("sample=subdir/subdir/sample.wav", "sample", "subdir/subdir/sample.wav");
+    memberTest("sample=subdir_underscore\\sample.wav", "sample", "subdir_underscore\\sample.wav");
+    memberTest("sample=subdir space\\sample.wav", "sample", "subdir space\\sample.wav");
+    memberTest("sample=subdir space\\sample.wav next_member=value", "sample", "subdir space\\sample.wav");
+    memberTest("sample=..\\Samples\\pizz\\a0_vl3_rr3.wav", "sample", "..\\Samples\\pizz\\a0_vl3_rr3.wav");
+    memberTest("sample=..\\Samples\\SMD Cymbals Stereo (Samples)\\Hi-Hat (Samples)\\01 Hat Tight 1\\RR1\\09_Hat_Tight_Cnt_RR1.wav", "sample", "..\\Samples\\SMD Cymbals Stereo (Samples)\\Hi-Hat (Samples)\\01 Hat Tight 1\\RR1\\09_Hat_Tight_Cnt_RR1.wav");
+}
+
+void parameterTest(const std::string& line, const std::string& opcode, const std::string& parameter)
+{
+    std::smatch parameterMatch;
+    auto found = std::regex_search(line, parameterMatch, SfzRegexes::opcodeParameters);
+    REQUIRE(found);
+    REQUIRE(parameterMatch[1] == opcode);
+    REQUIRE(parameterMatch[2] == parameter);
+}
+
+void parameterFail(const std::string &line)
+{
+    std::smatch parameterMatch;
+    auto found = std::regex_search(line, parameterMatch, SfzRegexes::opcodeParameters);
+    REQUIRE(!found);
+}
+
+TEST_CASE("Opcode parameter Regex", "Regex Tests")
+{
+    parameterTest("opcode_123", "opcode_", "123");
+    parameterTest("xfin_locc1", "xfin_locc", "1");
+    parameterTest("ampeg_hold_oncc24", "ampeg_hold_oncc", "24");
+    parameterTest("lfo02_phase_oncc135", "lfo02_phase_oncc", "135");
+    parameterFail("lfo01_freq");
+    parameterFail("ampeg_sustain");
 }
